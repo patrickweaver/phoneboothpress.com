1b86e5ef6a7598b4a8835d0673dc82a59ccb77de
Patrick Weaver <pjpwea@gmail.com>
2022-03-27 16:02:03 -0400

    Updates

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function (eleventyConfig) {
+  return {
+    templateFormats: [
+      "html",
+      "md",
+      "css",
+      "map",
+      "hbs",
+      "njk",
+      "gif",
+      "jpg",
+      "jpeg",
+      "png",
+      "mp3",
+      "mp4",
+      "pdf",
+      "eot",
+      "svg",
+      "ttf",
+      "woff",
+      "otf",
+      "ico",
+    ],
+  };
+};
